75f5bb909c1702261f85f090fed684c2f9bb6b64
JoelArturoGonzalez <L20100206@nlaredo.tecnm.mx>
2022-10-19 13:44:36 -0500

    Actualizacion

---
+++ Parcial2/SideBar js/Sidebar.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+
+    document.getElementById("btnMenu").addEventListener("click",function(){
+        document.getElementById("btn-menu").classList.toggle("escondido");
+    })
+
+    
+}
