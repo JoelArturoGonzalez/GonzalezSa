371b80445ea23634ae5e70b5f8d4657dcb9478ca
JoelArturoGonzalez <L20100206@nlaredo.tecnm.mx>
2022-11-17 18:54:14 -0500

    Update Consulta.php

--- Parcial3/ConsultaPhp/Consulta.php
+++ Parcial3/ConsultaPhp/Consulta.php
@@ -6,6 +6,11 @@ $password = "";
 
 $con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("No se pudo conectar al servidor");
 $consulta = "select * from tabla";
-$registros = mysqli_query($con,$consulta) or die ("problemas si")
+$registros = mysqli_query($con,$consulta) or die ("problemas si");
+
+$result = mysqli_fetch_all($registros);
+
+mysqli_close($con);
+echo json_encode($result);
 
 ?>
